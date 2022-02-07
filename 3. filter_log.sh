bd53322dbbbe9feda72897de2e7787acd0df0222
rabakuku <71357873+rabakuku@users.noreply.github.com>
2022-02-06 21:46:35 -0500

    Delete 3. filter_log.sh

--- 3. filter_log.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat /var/lib/docker/containers/91f325d4f0dc731b7b7e20ed40a8ecbd5ae0e0ca8c65af744233a957e0c29c79/91f325d4f0dc731b7b7e20ed40a8ecbd5ae0e0ca8c65af744233a957e0c29c79-json.log | grep "solved block" | sed 's/^.*\(".*Z\).*$/\1/' > /root/flask_project/textfile.txt
